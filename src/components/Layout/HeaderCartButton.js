76f79a9087ad9205c11910cfbfd2cf71f528cacc
dezfuture <agrim2000@gmail.com>
2021-05-29 14:40:06 +0530

    Added many of the Cart functionalities such as adding items, calculating total amount and many more using useContext and other React States

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 
 import CartIcon from "../Cart/CartIcon";
-import cartContext from "../../store/cart-context";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
-  const cartCtx = useContext(cartContext);
+  const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
